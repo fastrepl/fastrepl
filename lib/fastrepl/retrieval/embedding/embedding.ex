98c9fded15c390f9444366f9c5f21f8d37593e2c
yujong-lee <yujonglee@g.skku.edu>
2024-04-26 14:25:33 +0900

    add openai embedding with retry and truncate

--- lib/fastrepl/retrieval/embedding/embedding.ex
+++ lib/fastrepl/retrieval/embedding/embedding.ex
@@ -1,4 +1,3 @@
 defmodule Fastrepl.Retrieval.Embedding do
   @callback generate(String.t() | [String.t()]) :: {:ok, [[float()]]} | {:error, any()}
-  @callback generate!(String.t() | [String.t()]) :: [[float()]]
 end
---
+++ lib/fastrepl/retrieval/embedding/openai.ex
@@ -0,0 +1,69 @@
+defmodule Fastrepl.Retrieval.Embedding.OpenAI do
+  @behaviour Fastrepl.Retrieval.Embedding
+
+  use Retry
+  require Logger
+  alias Fastrepl.Tokenizer
+
+  @url "https://api.openai.com/v1/embeddings"
+  @model "text-embedding-3-small"
+  @dimensions 512
+  @max_input_tokens 8191
+  # @max_batch_size 2048
+
+  @spec generate(String.t() | [String.t()]) :: {:ok, [[float()]]} | {:error, any()}
+  def generate(texts) do
+    retry with:
+            exponential_backoff()
+            |> randomize
+            |> cap(1_000)
+            |> expiry(6_000),
+          atoms: [:error] do
+      request(texts)
+    after
+      {:ok, result} ->
+        embedding = result |> Enum.sort_by(& &1["index"]) |> Enum.map(& &1["embedding"])
+        {:ok, embedding}
+
+      :context_length_exceeded ->
+        tok = Tokenizer.load(:llama)
+
+        texts
+        |> Enum.map(&Tokenizer.truncate(&1, tok, @max_input_tokens - 200))
+        |> generate
+    else
+      error -> error
+    end
+  end
+
+  defp request(texts) do
+    api_key = Application.get_env(:langchain, :openai_key)
+
+    headers = [
+      {"Authorization", "Bearer #{api_key}"},
+      {"Content-Type", "application/json"}
+    ]
+
+    body = %{
+      input: texts,
+      model: @model,
+      encoding_format: "float",
+      dimensions: @dimensions
+    }
+
+    case Req.post(url: @url, headers: headers, json: body) do
+      {:ok, %{status: 200, body: %{"data" => data}}} ->
+        {:ok, data}
+
+      {:ok, %{body: %{"error" => %{"message" => message}}}} ->
+        if String.contains?(message, "maximum context length") do
+          :context_length_exceeded
+        else
+          {:error, message}
+        end
+
+      {:error, exception} ->
+        {:error, exception}
+    end
+  end
+end
