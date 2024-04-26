9d824b4886bc3e9f9094e01cf8b0ac92005ef600
yujong-lee <yujonglee@g.skku.edu>
2024-04-26 13:46:03 +0900

    add github login button

--- lib/fastrepl_web/live/main_live.ex
+++ lib/fastrepl_web/live/main_live.ex
@@ -1,11 +1,13 @@
 defmodule FastreplWeb.MainLive do
   use FastreplWeb, :live_view
+  import FastreplWeb.AuthComponents, only: [sign_in_with_github: 1]
 
   def render(assigns) do
     ~H"""
     <div>
       <span>Demo:</span>
       <.link href={~p"/demo/new"}>Go</.link>
+      <.sign_in_with_github />
     </div>
     """
   end
