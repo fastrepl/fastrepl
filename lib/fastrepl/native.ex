d5b97e381316bb388130b52e9b068dff0d371129
yujong-lee <yujonglee@g.skku.edu>
2024-04-26 13:54:49 +0900

    add spec in native module

--- lib/fastrepl/native.ex
+++ lib/fastrepl/native.ex
@@ -1,6 +1,8 @@
 defmodule Fastrepl.Native.RustChunker do
   use Rustler, otp_app: :fastrepl, crate: :rust_chunker
+  alias Fastrepl.Retrieval.Chunker.Chunk
 
+  @spec chunk_code(String.t(), String.t()) :: [Chunk.t()]
   def chunk_code(_path, _code), do: error()
   defp error(), do: :erlang.nif_error(:nif_not_loaded)
 end
