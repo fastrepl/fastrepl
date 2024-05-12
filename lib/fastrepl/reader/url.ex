88fa0031e7a2997414056f6ee2306c2fc8eb9fcd
yujong-lee <yujonglee@g.skku.edu>
2024-05-12 22:33:20 +0900

    github repo, issue, pr rendering support

--- lib/fastrepl/reader/url.ex
+++ lib/fastrepl/reader/url.ex
@@ -10,18 +10,43 @@ defmodule Fastrepl.Reader.URL do
     end
   end
 
-  defmemo get_commit(repo, hash) do
-    Github.get_commit!(repo, hash)
+  defmemo get_commit(repo_full_name, hash) do
+    Github.get_commit!(repo_full_name, hash)
+  end
+
+  defmemo get_issue(repo_full_name, issue_number) do
+    Github.get_issue!(repo_full_name, String.to_integer(issue_number))
+  end
+
+  defmemo list_issue_comments(repo_full_name, issue_number) do
+    Github.list_issue_comments!(repo_full_name, String.to_integer(issue_number))
   end
 
   def text_from_html(url) do
     html = html_from_url(url)
 
     cond do
-      is_github_commit_url?(url) ->
+      github_repo_url?(url) ->
+        case Regex.run(~r/([^\/]+\/[^\/]+)\/?$/, url) do
+          [_, repo_full_name] ->
+            article = html |> Floki.parse_document!() |> Floki.find("article")
+
+            """
+            [#{repo_full_name}]
+
+            ```README
+            #{Floki.text(article)}
+            ```
+            """
+
+          _ ->
+            ""
+        end
+
+      github_commit_url?(url) ->
         case Regex.run(~r/([^\/]+\/[^\/]+)\/commit\/(\w+)/, url) do
-          [_, repo, hash] ->
-            commit = get_commit(repo, hash)
+          [_, repo_full_name, hash] ->
+            commit = get_commit(repo_full_name, hash)
 
             diff =
               commit.files
@@ -29,6 +54,38 @@ defmodule Fastrepl.Reader.URL do
               |> Enum.join("\n\n")
 
             "#{commit.commit.message}\n\n#{diff}"
+
+          _ ->
+            ""
+        end
+
+      github_issue_url?(url) ->
+        case Regex.run(~r/([^\/]+\/[^\/]+)\/issues\/(\d+)/, url) do
+          [_, repo_full_name, issue_number] ->
+            issue = get_issue(repo_full_name, issue_number)
+            comments = list_issue_comments(repo_full_name, issue_number)
+
+            """
+            [Issue ##{issue_number}] #{issue.title}\n\n#{issue.body}
+
+            #{comments |> Enum.map(&Fastrepl.LLM.render/1) |> Enum.join("\n\n")}
+            """
+
+          _ ->
+            ""
+        end
+
+      github_pr_url?(url) ->
+        case Regex.run(~r/([^\/]+\/[^\/]+)\/pull\/(\d+)/, url) do
+          [_, repo_full_name, pr_number] ->
+            pr = get_issue(repo_full_name, pr_number)
+            comments = list_issue_comments(repo_full_name, pr_number)
+
+            """
+            [PR ##{pr_number}] #{pr.title}\n\n#{pr.body}
+
+            #{comments |> Enum.map(&Fastrepl.LLM.render/1) |> Enum.join("\n\n")}
+            """
 
           _ ->
             ""
@@ -57,8 +114,29 @@ defmodule Fastrepl.Reader.URL do
     |> Enum.reject(fn url -> url == "" end)
   end
 
-  def is_github_commit_url?(url) do
+  def github_repo_url?(url) do
+    case Regex.run(~r/github\.com\/[\w-]+\/[\w-]+\/?$/, url) do
+      nil -> false
+      _ -> true
+    end
+  end
+
+  defp github_commit_url?(url) do
     case Regex.run(~r/github\.com\/[\w-]+\/[\w-]+\/commit\/[0-9a-f]{40}/, url) do
+      nil -> false
+      _ -> true
+    end
+  end
+
+  defp github_issue_url?(url) do
+    case Regex.run(~r/github\.com\/[\w-]+\/[\w-]+\/issues\/\d+/, url) do
+      nil -> false
+      _ -> true
+    end
+  end
+
+  defp github_pr_url?(url) do
+    case Regex.run(~r/github\.com\/[\w-]+\/[\w-]+\/pull\/\d+/, url) do
       nil -> false
       _ -> true
     end
