78b0c1bf5ad1a97ed9a4f033fcbb11e8258469bc
yujong-lee <yujonglee@g.skku.edu>
2024-05-20 07:26:41 +0900

    remove grep module

--- lib/fastrepl/retrieval/grep.ex
+++
@@ -1,7 +0,0 @@
-defmodule Fastrepl.Retrieval.Grep do
-  alias Fastrepl.Native.CodeUtils
-
-  def grep_file(path, pattern) do
-    CodeUtils.grep_file(path, pattern)
-  end
-end
--- lib/fastrepl/retrieval/tool/keyword_search.ex
+++ lib/fastrepl/retrieval/tool/keyword_search.ex
@@ -2,14 +2,14 @@ defmodule Fastrepl.Retrieval.Tool.KeywordSearch do
   @behaviour Fastrepl.Retrieval.Tool
 
   alias Fastrepl.FS
-  alias Fastrepl.Retrieval.Grep
+  alias Fastrepl.Native.CodeUtils
   alias Fastrepl.Retrieval.Result
 
   def run(%{"query" => query}, %{root_path: root_path}) do
     root_path
     |> FS.list_informative_files()
     |> Enum.map(fn path ->
-      lines = Grep.grep_file(path, query)
+      lines = CodeUtils.grep_file(path, query)
 
       if Enum.empty?(lines),
         do: nil,
