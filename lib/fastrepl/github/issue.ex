5a8f7fc751d470a85e88f338805518f917b30291
yujonglee <yujonglee.dev@gmail.com>
2024-05-22 10:54:23 +0900

    update github utils and struct

--- lib/fastrepl/github/issue.ex
+++ lib/fastrepl/github/issue.ex
@@ -1,9 +1,12 @@
 defmodule Fastrepl.Github.Issue do
-  defstruct [:is_pr, :title, :number, :body, :url, :comments]
+  defstruct [:id, :node_id, :repo_full_name, :is_pr, :title, :number, :body, :url, :comments]
 
   alias Fastrepl.Github.Issue.Comment
 
   @type t :: %__MODULE__{
+          id: integer(),
+          node_id: String.t(),
+          repo_full_name: String.t(),
           is_pr: boolean(),
           title: String.t(),
           number: String.t(),
@@ -16,14 +19,17 @@ defmodule Fastrepl.Github.Issue do
     [owner, repo] = String.split(repo_full_name, "/")
 
     with {:ok, issue} <- GitHub.Issues.get(owner, repo, issue_number),
-         {:ok, comments} <- Comment.from(repo_full_name, issue_number) do
+         {:ok, comments} <- Comment.list_from(repo_full_name, issue_number) do
       {:ok,
        %__MODULE__{
+         id: issue.id,
+         node_id: issue.node_id,
+         repo_full_name: repo_full_name,
+         is_pr: String.contains?(issue.html_url, "pull"),
          title: issue.title,
          number: issue.number,
          body: issue.body,
          url: issue.html_url,
-         is_pr: String.contains?(issue.html_url, "pull"),
          comments: comments
        }}
     else
@@ -88,38 +94,63 @@ defmodule Fastrepl.Github.Issue do
     |> Task.await_many(10_000)
     |> Enum.reject(fn {_, issues} -> Enum.empty?(issues) end)
   end
+
+  def set_labels(%__MODULE__{} = issue, labels) do
+    [owner, repo] = String.split(issue.repo_full_name, "/")
+
+    GitHub.Issues.set_labels(owner, repo, issue.number, labels)
+  end
 end
 
 defmodule Fastrepl.Github.Issue.Comment do
-  defstruct [:is_pr, :body, :issue_url, :user_name]
+  defstruct [:id, :node_id, :repo_full_name, :body, :issue_url, :is_pr, :user_name]
 
   @type t :: %__MODULE__{
+          id: integer(),
+          node_id: String.t(),
+          repo_full_name: String.t(),
           is_pr: boolean(),
           body: String.t(),
           issue_url: String.t(),
           user_name: String.t()
         }
 
-  def from(repo_full_name, issue_number) do
+  def from(comment, repo_full_name) do
+    %__MODULE__{
+      id: comment.id,
+      node_id: comment.node_id,
+      repo_full_name: repo_full_name,
+      body: comment.body,
+      issue_url: comment.html_url,
+      is_pr: String.contains?(comment.html_url, "pull"),
+      user_name: comment.user.login
+    }
+  end
+
+  def list_from(repo_full_name, issue_number) do
     [owner, repo] = String.split(repo_full_name, "/")
 
     case GitHub.Issues.list_comments(owner, repo, issue_number, page: 1, per_page: 50) do
-      {:ok, comments} ->
-        comments =
-          comments
-          |> Enum.map(fn comment ->
-            %__MODULE__{
-              body: comment.body,
-              issue_url: comment.html_url,
-              is_pr: String.contains?(comment.html_url, "pull"),
-              user_name: comment.user.login
-            }
-          end)
+      {:ok, comments} -> {:ok, Enum.map(comments, &from(&1, repo_full_name))}
+      {:error, error} -> {:error, error}
+    end
+  end
 
-        {:ok, comments}
+  def create(repo_full_name, issue_number, body) do
+    [owner, repo] = String.split(repo_full_name, "/")
 
-      {:error, error} ->
-        {:error, error}
+    case GitHub.Issues.create_comment(owner, repo, issue_number, body) do
+      {:ok, comment} -> {:ok, from(comment, repo_full_name)}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  def update(%__MODULE__{} = comment, body) do
+    [owner, repo] = String.split(comment.repo_full_name, "/")
+
+    case GitHub.Issues.update_comment(owner, repo, comment.id, body) do
+      {:ok, updated_comment} -> {:ok, from(updated_comment, comment.repo_full_name)}
+      {:error, error} -> {:error, error}
     end
   end
 end
