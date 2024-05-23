2fc703e3f1d512d5ad3b274d136bfbfcb39d7020
yujonglee <yujonglee.dev@gmail.com>
2024-05-23 10:02:36 +0900

    update iex

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,9 @@
 import Ecto.Query
+alias Fastrepl.Repo
 
-alias Fastrepl.{Repo}
+alias Identity.User
+alias Fastrepl.Accounts.Member
+alias Fastrepl.Accounts.Account
+alias Fastrepl.Billings.Billing
+
+alias Fastrepl.Github
